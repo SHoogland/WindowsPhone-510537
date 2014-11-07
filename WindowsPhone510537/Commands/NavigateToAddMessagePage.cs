0103e7c8bafbbdc398c060492ee97ac883a2fd6d
Stephan Hoogland <stephan@shoogland.com>
2014-11-07 14:11:23 +0100

    added addpage including return catch button thingie

---
+++ WindowsPhone510537/Commands/NavigateToAddMessagePage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using WindowsPhone510537.Views;
+
+namespace WindowsPhone510537.Commands {
+    public sealed class NavigateToAddMessagePage : ICommand {
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+        public void Execute(object parameter) {
+            ((Frame)Window.Current.Content).Navigate(typeof(AddMessagePage));
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- WindowsPhone510537/ViewModels/MainViewModel.cs
+++ WindowsPhone510537/ViewModels/MainViewModel.cs
@@ -4,11 +4,16 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsPhone510537.Commands;
 using WindowsPhone510537.Models;
 using WindowsPhone510537.Services;
 
 namespace WindowsPhone510537.ViewModels {
     public sealed class MainViewModel {
+        public MainViewModel() {
+            NavigateToAddMessagePageCommand = new NavigateToAddMessagePage();
+        }
         public ObservableCollection<Message> Messages { get { return DataSource.Messages; } }
+        public NavigateToAddMessagePage NavigateToAddMessagePageCommand { get; private set; }
     }
 }
